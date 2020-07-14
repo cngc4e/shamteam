a8c7c73afd023475ec2fba75bedffd36451c48a0
cngc4e <cngc4e@gmail.com>
2020-07-14 23:24:16 +0800

    build.py: recursive inclusion

--- build.py
+++ build.py
@@ -2,12 +2,19 @@ import re
 
 template = open("module.lua", "r", encoding="utf-8").read()
 
-
-def getContent(match):
+def getContentFromFile(match):
     return open(match.group(1), "r", encoding="utf-8").read()
-    
+
 reg = re.compile(r'@include (\S+)', re.S)
-template = reg.sub(getContent, template)
+def expand(content):
+    if '@include' in content:
+        print("Aa")
+        content = reg.sub(getContentFromFile, content)
+        return expand(content)
+    else:
+        return content
+
+template = expand(template)
 
 with open("shamteam.lua", "w", encoding="utf-8") as f:
     f.write(template)
